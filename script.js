d754c38d091d6a28c55f0d9d8f88873c441c1964
Patric <patric.personal99@gmail.com>
2024-02-16 06:28:48 +0100

    move from react to plain html & tailwind

---
+++ script.js
@@ -0,0 +1,69 @@
+const info = {
+    author: {
+        name: "Patric",
+        lastname: "Pintescul",
+    },
+    experience: {
+        desktop: {
+            python: new Date(2019),
+            java: new Date(2019),
+            cCpp: new Date(2020),
+            cSharp: new Date(2020),
+            swift: new Date(2021),
+            rust: new Date(2022),
+            pascal: new Date(2023),
+            assembly: new Date(2023)
+        },
+        mobile: {
+            swift: new Date(2023),
+            kotlin: new Date(2020),
+        },
+        web: {
+            htmlcss: new Date(2019),
+            javascript: new Date(2020),
+            react: new Date(2021),
+        }
+    },
+    courses: [
+        {
+            title: "Generative AI and machine learning",
+            at: "Google LLC"
+        },
+        {
+            title: "ML Engineer",
+            at: "Google LLC"
+        },
+        {
+            title: "TensorFlow developer",
+            at: "Google LLC"
+        },
+        {
+            title: "React Native Specialization",
+            at: "Meta LLC"
+        },
+        {
+            title: "Google UX design",
+            at: "Google LLC"
+        },
+        {
+            title: "Google data analytics",
+            at: "Google LLC"
+        },
+        {
+            title: "Meta front-end developer",
+            at: "Meta LLC"
+        },
+        {
+            title: "Advanced react",
+            at: "Meta LLC"
+        },
+        {
+            title: "Advanced Swift",
+            at: "Meta LLC"
+        },
+        {
+            title: "the full stack",
+            at: "Meta LLC"
+        }
+    ]
+}
